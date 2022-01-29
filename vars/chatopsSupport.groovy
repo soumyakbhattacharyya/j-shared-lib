0f46dfbb9f81640f57e55aae57103552f7ebd960
soumyak_bhattacharyya <bhattacharyya.soumyak@gmail.com>
2022-01-29 22:44:18 +0530

    updated to ensure conditional termination

--- vars/chatopsSupport.groovy
+++ vars/chatopsSupport.groovy
@@ -1,18 +1,16 @@
 def call(Map args) {
     if (args.action == 'check') {
-	    println "within check method"
-        return check()
+	    return check()
     }
     if (args.action == 'postProcess') {
         return postProcess()
     }
-    error 'ciSkip has been called without valid arguments'
+    error 'chatopsSupport has been called without valid arguments'
 }
 
 def check() {
     env.DESTROY_ENVIRONMENT = "true" // flag to destroy test environment as a default behavior
     result = sh (script: "git log -1 | grep 'KEEP_ON_FAILURE'", returnStatus: true)
-	println "result " + result
     if (result == 0) {
         env.DESTROY_ENVIRONMENT = "false" // keep test environment 
         println "'[KEEP_ON_FAILURE]' found in git commit message. Keeping test environment intact."
