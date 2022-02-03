62841fd6f7d88e503f7aa72f1742b2ed2760fd75
soumyak <bhattacharyya.soumyak@gmail.com>
2022-02-03 23:29:46 +0530

    Update chatopsSupport.groovy

--- vars/chatopsSupport.groovy
+++ vars/chatopsSupport.groovy
@@ -20,14 +20,14 @@ def comment(){
     def user = "soumyakbhattacharyya"
     def tokenId = "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"
     def url = "https://api.github.com/repos/soumyakbhattacharyya/to-be-used-for-jenkins-poc/issues/1/comments"
-    def command = "curl -i -X POST -H \"Accept: application/vnd.github.v3+json\" -u $user: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 $url -d '{\"body\":\"body12345\"}'"
+    def command = "curl -i -X POST -H \"Accept: application/vnd.github.v3+json\" -u $user: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 $url -d '{\"body\":\"body12345\"}'"
     println command	
     println command.execute().text	
 }
 
 def check() {
     def user = "soumyakbhattacharyya"
-    def tokenId = "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"
+    def tokenId = "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"
     def url = "https://api.github.com/repos/soumyakbhattacharyya/to-be-used-for-jenkins-poc/issues/1/comments"
     def command = "curl -i -u $user:#tokenId $url"
     
