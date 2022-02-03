248797f9ab3dc43dcdb4b8fadf3376a0e20a811f
soumyak <bhattacharyya.soumyak@gmail.com>
2022-02-03 21:38:32 +0530

    Update chatopsSupport.groovy

--- vars/chatopsSupport.groovy
+++ vars/chatopsSupport.groovy
@@ -2,6 +2,29 @@ import net.sf.json.groovy.JsonSlurper
 
 def call(Map args) {
 	
+    if (args.action == 'comment') {
+	    return comment()
+    }	
+	
+    if (args.action == 'check') {
+	    return check()
+    }
+    if (args.action == 'terminateEnvironment') {
+        return terminateEnvironment()
+    }
+    error 'chatopsSupport has been called without valid arguments'
+}
+
+def comment(){
+ 
+    def user = "soumyakbhattacharyya"
+    def tokenId = "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"
+    def url = "https://api.github.com/repos/soumyakbhattacharyya/to-be-used-for-jenkins-poc/issues/1/comments"
+    def command = "curl -i -u $user:#tokenId $url -d '{\"body\":\"please inform if environment needs to be kept intact\"}'"
+    println command.execute().text	
+}
+
+def check() {
     def user = "soumyakbhattacharyya"
     def tokenId = "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"
     def url = "https://api.github.com/repos/soumyakbhattacharyya/to-be-used-for-jenkins-poc/issues/1/comments"
@@ -19,23 +42,6 @@ def call(Map args) {
 	    break
 	    
        Thread.sleep(5000)	    
-    }
-
-    if (args.action == 'check') {
-	    return check()
-    }
-    if (args.action == 'terminateEnvironment') {
-        return terminateEnvironment()
-    }
-    error 'chatopsSupport has been called without valid arguments'
-}
-
-def check() {
-    env.DESTROY_ENVIRONMENT = "true" // flag to destroy test environment as a default behavior
-    result = sh (script: "git log -1 | grep 'KEEP_ON_FAILURE'", returnStatus: true)
-    if (result == 0) {
-        env.DESTROY_ENVIRONMENT = "false" // keep test environment 
-        println "'[KEEP_ON_FAILURE]' found in git commit message. Keeping test environment intact."
     }
 }
 
