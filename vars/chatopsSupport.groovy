c9c16eb7f1e8c5f11a00948addf136cb7782179c
soumyak <bhattacharyya.soumyak@gmail.com>
2022-02-07 17:24:18 +0530

    Update chatopsSupport.groovy

--- vars/chatopsSupport.groovy
+++ vars/chatopsSupport.groovy
@@ -22,7 +22,7 @@ def comment(token){
     def user = "soumyakbhattacharyya"
     def tokenId = token
     def url = "https://api.github.com/repos/soumyakbhattacharyya/to-be-used-for-jenkins-poc/issues/1/comments"
-    def command = "curl -i -X POST -H \"Accept: application/vnd.github.v3+json\" -u $user:#token $url -d '{\"body\":\"Commenting from Jenkins\"}'"
+    def command = "curl -i -X POST -H \"Accept: application/vnd.github.v3+json\" -u $user:$token $url -d '{\"body\":\"Commenting from Jenkins\"}'"
     println command	
     println command.execute().text	
 }
